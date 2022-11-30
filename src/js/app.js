e37d63692d2c48297ff1f710e5ea4af1e7e5523b
Петър Стоев <petarstoev0505@gmail.com>
2022-11-30 21:08:41 +0200

    v1

--- src/js/app.js
+++ src/js/app.js
@@ -7,4 +7,20 @@ window.addEventListener("DOMContentLoaded", () => {
   button.addEventListener("click", () => {
     alert("💣");
   });
+
+  const main = document.querySelector(".main");
+  const imageDiv = document.createElement("div");
+  imageDiv.className = "image";
+  main.appendChild(imageDiv);
+  const image = document.createElement("img");
+  image.src="https://www.freeiconspng.com/thumbs/cat-png/cat-png-17.png"
+  imageDiv.appendChild(image);
+
+  document.querySelector(".image").addEventListener("click", () =>{
+    imageDiv.style.scale = "2"
+  })
+  console.log(imageDiv);
+  console.log(main);
 });
+
+
